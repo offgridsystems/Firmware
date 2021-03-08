1c30be65f1ca653298dbce688c561e847047ab82
Offgrid Systems <offgridsystems@users.noreply.github.com>
2021-03-08 12:19:36 -0800

    Delete soc-btmesh-supervisor directory

--- soc-btmesh-supervisor/GNU ARM v7.2.1 - Default/gatt_db.d
+++
@@ -1,7 +0,0 @@
-gatt_db.o: ../gatt_db.c \
- C:\Users\nick\git\dkblock\soc-btmesh-supervisor\protocol\bluetooth\bt_mesh\inc\common/bg_gattdb_def.h \
- C:\Users\nick\git\dkblock\soc-btmesh-supervisor\protocol\bluetooth\bt_mesh\inc\common/bg_types.h
-
-C:\Users\nick\git\dkblock\soc-btmesh-supervisor\protocol\bluetooth\bt_mesh\inc\common/bg_gattdb_def.h:
-
-C:\Users\nick\git\dkblock\soc-btmesh-supervisor\protocol\bluetooth\bt_mesh\inc\common/bg_types.h:
--- soc-btmesh-supervisor/GNU ARM v7.2.1 - Default/protocol/bluetooth/bt_mesh/src/bg_application_properties.d
+++
@@ -1,5 +0,0 @@
-protocol/bluetooth/bt_mesh/src/bg_application_properties.o: \
- ../protocol/bluetooth/bt_mesh/src/bg_application_properties.c \
- C:\Users\nick\git\dkblock\soc-btmesh-supervisor\platform\bootloader\api/application_properties.h
-
-C:\Users\nick\git\dkblock\soc-btmesh-supervisor\platform\bootloader\api/application_properties.h:
